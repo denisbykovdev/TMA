c42ba9cfb26e276076e4b4aab6fb5e921507e589
denisbykovdev <denysbykov@icloud.com>
2024-10-18 08:22:00 +0300

    eruda

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import WebApp from '@twa-dev/sdk';
+import eruda from 'eruda'
+
+eruda.init();
 
 WebApp.ready();
 
